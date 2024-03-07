39f401eeecd1a4261ee37e7f00248481bbe3174d
akashm-18 <akashmurugan338@gmail.com>
2024-03-07 17:53:26 +0530

    db connected .env ignored

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 const app = express()
 
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.log(err))
+
 app.listen(3000,() => {
-    console.log('Server running on the port 3000!!')
+    console.log('Server running on the port 3000 !!')
 })
