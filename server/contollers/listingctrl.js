90012e6ea2b8ffd9dab391a459548a68e66be9dc
akashm-18 <akashmurugan338@gmail.com>
2024-03-10 23:41:25 +0530

    completed delete user listing functionality

--- server/contollers/listingctrl.js
+++ server/contollers/listingctrl.js
@@ -1,4 +1,5 @@
 import Listing from '../modals/ListingModal.js'
+import { errorHandler } from '../utilis/error.js'
 
 export const createListing = async (req,res,next) => {
     try {
@@ -7,4 +8,21 @@ export const createListing = async (req,res,next) => {
     } catch (error) {
         next(error)
     }
+}
+
+export const deleteListing = async (req , res , next) => {
+        const id = req.params.id
+        const listing = await Listing.findById(id)
+        if (!listing) return next(errorHandler(404,'Listing not found!'))
+
+        if (req.user.id !== listing.userRef){
+            return next(errorHandler('You can only delete your listings!'))
+        }
+        try {
+            await Listing.findByIdAndDelete(id)
+            res.status(201).json('Listing deleted !')
+        
+        } catch (error) {
+            next(error)
+        }
 }
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../utilis/verifyToken.js'
-import { createListing } from '../contollers/listingctrl.js';
+import { createListing, deleteListing } from '../contollers/listingctrl.js';
 
 
 const router = express.Router()
 
 router.post('/create' , verifyToken , createListing)
+router.delete('/delete/:id' , verifyToken , deleteListing)
 
 
 export default router;
